245fa4d92094d1e17b08ea8c7f6dfb4b5e4b5714
tuanfam <tuanphamm198@gmail.com>
2024-09-15 20:23:58 +0900

    fist push

--- src/listTraining/Employee.java
+++ src/listTraining/Employee.java
@@ -2,4 +2,62 @@ package listTraining;
 
 public class Employee {
 	// viet tiep...
+	private String ten;
+	private int tuoi;
+	boolean gioiTinh;
+	private String queQuan;
+	private double luong;
+	private String chucVu;
+
+	public String getTen() {
+		return ten;
+	}
+
+	public int getTuoi() {
+		return tuoi;
+	}
+
+	public String getQueQuan() {
+		return queQuan;
+	}
+
+	public double getLuong() {
+		return luong;
+	}
+
+	public String getChucvu() {
+		return chucVu;
+	}
+
+	public boolean isGioiTing() {
+		return gioiTinh;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+
+	public void setGioiTinh(String gioiTinh) {
+		this.gioiTinh = gioiTinh.equals("true");
+	}
+
+	public void setQueQuan(String queQuan) {
+		this.queQuan = queQuan;
+	}
+
+	public void setLuong(double luong) {
+		this.luong = luong;
+	}
+
+	public void setChucVu(String chucVu) {
+		this.chucVu = chucVu;
+	}
+
+	public String getGioiTinhString() {
+		return gioiTinh ? "nam" : "nu";
+	}
 }
--- src/listTraining/Main.java
+++ src/listTraining/Main.java
@@ -1,7 +1,125 @@
 package listTraining;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
-		// viet tiep...
+
+		Employee hito1 = new Employee();
+		hito1.setTen("Hung");
+		hito1.setTuoi(35);
+		hito1.setGioiTinh("true");
+		hito1.setQueQuan("Ha Noi");
+		hito1.setLuong(30.0);
+		hito1.setChucVu("truong Phong IT");
+
+		Employee hito2 = new Employee();
+		hito2.setTen("Huong");
+		hito2.setTuoi(32);
+		hito2.setGioiTinh("false");
+		hito2.setQueQuan("Nghe An");
+		hito2.setLuong(20.0);
+		hito2.setChucVu("Ke Toan");
+
+		Employee hito3 = new Employee();
+		hito3.setTen("Lan");
+		hito3.setTuoi(28);
+		hito3.setGioiTinh("false");
+		hito3.setQueQuan("Hai Phong");
+		hito3.setLuong(19);
+		hito3.setChucVu("maketing");
+
+		Employee hito4 = new Employee();
+		hito4.setTen("Duong");
+		hito4.setTuoi(31);
+		hito4.setGioiTinh("true");
+		hito4.setQueQuan("Phu Tho");
+		hito4.setLuong(25);
+		hito4.setChucVu("ki Su IT");
+
+		Employee hito5 = new Employee();
+		hito5.setTen("Nam");
+		hito5.setTuoi(29);
+		hito5.setGioiTinh("true");
+		hito5.setQueQuan("Ha Noi");
+		hito5.setLuong(21);
+		hito5.setChucVu("ki Su IT");
+
+		Employee hito6 = new Employee();
+		hito6.setTen("Vy");
+		hito6.setTuoi(21);
+		hito6.setGioiTinh("false");
+		hito6.setQueQuan("Ha Noi");
+		hito6.setLuong(5);
+		hito6.setChucVu("intern");
+
+		String ten = hito1.getTen();
+		int tuoi = hito1.getTuoi();
+		String gioitinh = hito1.getGioiTinhString();
+		String queQuan = hito1.getQueQuan();
+		double luong = hito1.getLuong();
+		String chucVu = hito1.getChucvu();
+
+		String ten1 = hito2.getTen();
+		int tuoi1 = hito2.getTuoi();
+		String gioitinh1 = hito2.getGioiTinhString();
+		String queQuan1 = hito2.getQueQuan();
+		double luong1 = hito2.getLuong();
+		String chucVu1 = hito2.getChucvu();
+
+		String ten2 = hito3.getTen();
+		int tuoi2 = hito3.getTuoi();
+		String gioitinh2 = hito3.getGioiTinhString();
+		String queQuan2 = hito3.getQueQuan();
+		double luong2 = hito3.getLuong();
+		String chucVu2 = hito3.getChucvu();
+
+		String ten3 = hito4.getTen();
+		int tuoi3 = hito4.getTuoi();
+		String gioitinh3 = hito4.getGioiTinhString();
+		String queQuan3 = hito4.getQueQuan();
+		double luong3 = hito4.getLuong();
+		String chucVu3 = hito4.getChucvu();
+
+		String ten4 = hito5.getTen();
+		int tuoi4 = hito5.getTuoi();
+		String gioitinh4 = hito4.getGioiTinhString();
+		String queQuan4 = hito5.getQueQuan();
+		double luong4 = hito5.getLuong();
+		String chucVu4 = hito5.getChucvu();
+
+		String ten5 = hito6.getTen();
+		int tuoi5 = hito6.getTuoi();
+		String gioitinh5 = hito6.getGioiTinhString();
+		String queQuan5 = hito6.getQueQuan();
+		double luong5 = hito6.getLuong();
+		String chucVu5 = hito6.getChucvu();
+
+		System.out.println(ten + " " + tuoi + " tuoi " + gioitinh + " " + queQuan + " " + luong + " trieu " + chucVu);
+		System.out.println(
+				ten1 + " " + tuoi1 + " tuoi " + gioitinh1 + " " + queQuan1 + " " + luong1 + " trieu " + chucVu1);
+		System.out.println(
+				ten2 + " " + tuoi2 + " tuoi " + gioitinh2 + " " + queQuan2 + " " + luong2 + " trieu " + chucVu2);
+		System.out.println(
+				ten3 + " " + tuoi3 + " tuoi " + gioitinh3 + " " + queQuan3 + " " + luong3 + " trieu " + chucVu3);
+		System.out.println(
+				ten4 + " " + tuoi4 + " tuoi " + gioitinh4 + " " + queQuan4 + " " + luong4 + " trieu " + chucVu4);
+		System.out.println(
+				ten5 + " " + tuoi5 + " tuoi " + gioitinh5 + " " + queQuan5 + " " + luong5 + " trieu " + chucVu5);
+
+		List<Employee> employees = Arrays.asList(hito1, hito2, hito3, hito4, hito5, hito6);
+
+//		in lương trên 30tr
+		System.out.println("danh sach nhan vien luong tren 20tr:");
+		for (Employee employee : employees) {
+			if (employee.getLuong() > 20.0) {
+				System.out.println(employee.getTen() + ": " + "Luong " + employee.getLuong() + "Trieu");
+			}
+		}
 	}
+//	In ra tổng số nhân viên nam cùng với tên những nhân viên nam:
+	
+//	System.out.println("");
+	
 }

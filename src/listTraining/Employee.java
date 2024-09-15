a847f33162d75abd01a9cbf104d7a166860c050a
tuanfam <tuanphamm198@gmail.com>
2024-09-15 19:26:21 +0900

    first commit

---
+++ src/listTraining/Employee.java
@@ -0,0 +1,5 @@
+package listTraining;
+
+public class Employee {
+	// viet tiep...
+}
---
+++ src/listTraining/Main.java
@@ -0,0 +1,7 @@
+package listTraining;
+
+public class Main {
+	public static void main(String[] args) {
+		// viet tiep...
+	}
+}
